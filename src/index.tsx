8a953d648221ae717379b115134bec00de62ed57
Iago William <iagowilliam@hotmail.com>
2022-08-04 15:24:24 -0300

    hook finalizado

--- src/index.tsx
+++ src/index.tsx
@@ -38,10 +38,10 @@ createServer({
       return this.schema.all('transaction');
     })
 
-    this.post('transactions', (schema, request) => {
+    this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
-      return data.schema.create('transaction', data)
+      return schema.create('transaction', data)
     })
   }
 })
